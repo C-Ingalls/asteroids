f8823cb3787ca5eeb85a96eb435be819b89783e3
C-Ingalls <ingallsdchristopher@gmail.com>
2025-12-26 00:36:31 -0600

    add player object

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -13,12 +14,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
