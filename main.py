001e5c9b4223498dbc57e63f9210111105b634c8
C-Ingalls <ingallsdchristopher@gmail.com>
2025-12-26 01:25:37 -0600

    make player object work

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         log_state()
@@ -22,7 +27,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
 
         pygame.display.flip()
         clock.tick(60)
